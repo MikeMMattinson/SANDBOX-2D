452c5fc6202149ddf2a5f774521275915e5b0e45
Mike Mattinson <mike.mattinson@gmail.com>
2024-05-19 16:29:52 -0400

    StarMgr #2

--- Assets/Scripts/_03StarMgr/SpiralGalaxy.cs
+++ Assets/Scripts/_03StarMgr/SpiralGalaxy.cs
@@ -6,6 +6,8 @@ public class SpiralGalaxy : Galaxy
 {
     public GameObject[] starPrefab;
     public int starCount;
+    public float centralCluster = .75f;
+    public int numberOfArms = 3;
     public float starScale;
     public float factorStarScale;
     public int starLayer;
@@ -13,36 +15,60 @@ public class SpiralGalaxy : Galaxy
     public int indexGalaxyType = 1;
     public float worldWidth = 20;
     public float worldHeight = 20;
+    public bool DebugMode = false;
 
     public override void CreateAndPosition()
     {
-        //og the creation for demonstration purposes
-        Debug.Log($"Created a galaxy of type: {this.GetType().Name}");
+        if(DebugMode) Debug.Log($"Created a galaxy of type: {this.GetType().Name}");
 
-        // create stars
-        for (int i = 0; i < starCount; i++)
+        int centralStarCount = (int)(starCount * centralCluster); // 80% of stars
+        int spiralStarCount = starCount - centralStarCount; // remaining 20%
+
+        // Create central stars
+        for (int i = 0; i < centralStarCount; i++)
         {
-            // get a random star prefab
             GameObject starPrefab = this.starPrefab[UnityEngine.Random.Range(0, this.starPrefab.Length)];
-
-            // create a star
-            GameObject star = Instantiate(starPrefab, RandomPosition(), Quaternion.identity, this.transform);
-
-            // get the star script
-            Star starScript = star.GetComponent<Star>();
-
-            // create and position the star
-            //starScript.CreateAndPosition(starScale, factorStarScale, starLayer, galaxyRadius);
-
-            // Set the scale and layer of the star
-            // TODO test to see if these can be set in the prefab or Star.cs script
-            // randomize the default scale of the star to within a range +/- .05 of the default scale
-            float randomScale = Random.Range((starScale - factorStarScale), (starScale + factorStarScale));
-            star.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
-
-            // Set the layer of the star
-            star.layer = starLayer;
+            GameObject star = Instantiate(starPrefab, RandomCentralPosition(), Quaternion.identity, this.transform);
+            SetupStar(star);
         }
+
+        // Create spiral stars
+        for (int i = 0; i < spiralStarCount; i++)
+        {
+            GameObject starPrefab = this.starPrefab[UnityEngine.Random.Range(0, this.starPrefab.Length)];
+            GameObject star = Instantiate(starPrefab, RandomSpiralPosition(i, spiralStarCount), Quaternion.identity, this.transform);
+            SetupStar(star);
+        }
+    }
+
+    Vector3 RandomCentralPosition()
+    {
+        // Generate a position closer to the center
+        float radius = galaxyRadius * 0.2f; // Adjust as needed
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        float distance = Random.Range(0, radius);
+        return new Vector3(distance * Mathf.Cos(angle), distance * Mathf.Sin(angle), 0);
+    }
+
+    Vector3 RandomSpiralPosition(int index, int totalSpiralStars)
+    {
+        float armSeparation = (2 * Mathf.PI) / numberOfArms; // Separation between arms in radians
+        float angleIncrement = 0.5f; // Adjust for tighter or looser spirals
+
+        // Calculate the position for this star in its arm
+        float armIndex = index % numberOfArms; // Determine which arm this star belongs to
+        float distance = (index / (float)totalSpiralStars) * galaxyRadius; // Linear distribution of stars along the arm
+        float angle = armIndex * armSeparation + distance * angleIncrement; // Adjust angle based on arm and position along the arm
+
+        return new Vector3(distance * Mathf.Cos(angle), distance * Mathf.Sin(angle), 0);
+    }
+
+    void SetupStar(GameObject star)
+    {
+        // Set the scale and layer of the star
+        float randomScale = Random.Range((starScale - factorStarScale), (starScale + factorStarScale));
+        star.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+        star.layer = starLayer;
     }
 
     // random position function
